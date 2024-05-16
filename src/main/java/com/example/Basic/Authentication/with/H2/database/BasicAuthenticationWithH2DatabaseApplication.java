0fbb0cec30029b13fc114b1eec17f8943bf533ae
Arun <arunkumard0606@gmail.com>
2024-05-16 19:52:43 +0530

    Initial commit

---
+++ src/main/java/com/example/Basic/Authentication/with/H2/database/BasicAuthenticationWithH2DatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Basic.Authentication.with.H2.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthenticationWithH2DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAuthenticationWithH2DatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Basic/Authentication/with/H2/database/BasicAuthenticationWithH2DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Basic.Authentication.with.H2.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthenticationWithH2DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
